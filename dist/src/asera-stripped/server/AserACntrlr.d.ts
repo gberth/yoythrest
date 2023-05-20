8382937379266d2e595e425cdee3faaa91cdda5e
gunnar <gunnar.berthelsen@amedia.no>
2023-05-20 21:26:29 +0200

    save0

--- dist/src/asera-stripped/server/AserACntrlr.d.ts
+++
@@ -1,5 +0,0 @@
-import { StreamDef } from "./types";
-import AserADispatcher from "./AserADispatcher";
-export default class AserACntrlr extends AserADispatcher {
-    constructor(streamDef: StreamDef, outputStream: AserADispatcher, motherId: string);
-}
